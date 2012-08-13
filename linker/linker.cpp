2ed37e5573e63a851c4848121a8659eb409f1919
David Ng <dave@codeaurora.org>
2012-08-13 16:13:16 -0700

    linker: Increase maximum shared libraries loaded to 192
    Increase the maximum number of shared libraries loaded per
    process/SO_MAX to 192 from 128.  The mediaserver process
    can load more than 128 shared libraries due to media HAL
    and associated libraries.
    
    CRs-Fixed: 387489
    Change-Id: I21988cca7234f208ae80e1dff9a404458cb604a7

--- linker/linker.cpp
+++ linker/linker.cpp
@@ -51,7 +51,7 @@
 #include "linker_phdr.h"
 
 #define ALLOW_SYMBOLS_FROM_MAIN 1
-#define SO_MAX 128
+#define SO_MAX 192
 
 /* Assume average path length of 64 and max 8 paths */
 #define LDPATH_BUFSIZE 512
